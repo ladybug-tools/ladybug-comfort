18e393d435260f3eed9c53cd5c2963f5c3c46ddc
Matthew <mattblodgett94@gmail.com>
2019-10-09 19:16:19 -0400

    Add humidex degree of comfort function and tests

--- tests/humidex_test.py
+++ tests/humidex_test.py
@@ -1,7 +1,7 @@
 # coding utf-8
 import pytest
 
-from ladybug_comfort.humidex import humidex
+from ladybug_comfort.humidex import humidex, humidex_degree_of_comfort
 
 
 def test_humidex():
@@ -11,3 +11,18 @@ def test_humidex():
     assert humidex(28, 20) == pytest.approx(35.56977, rel=1e-3)
     assert humidex(28, 26) == pytest.approx(41.45899, rel=1e-3)
     assert humidex(34, 25) == pytest.approx(46.33840, rel=1e-3)
+
+
+def test_humidex_degree_of_comfort():
+    """Test the humidex degree of comfort thresholds"""
+    assert humidex_degree_of_comfort(0.0) == 0
+    assert humidex_degree_of_comfort(19.0) == 0
+    assert humidex_degree_of_comfort(19.9) == 0
+    assert humidex_degree_of_comfort(20.0) == 1
+    assert humidex_degree_of_comfort(29.9) == 1
+    assert humidex_degree_of_comfort(30.0) == 2
+    assert humidex_degree_of_comfort(39.9) == 2
+    assert humidex_degree_of_comfort(40.0) == 3
+    assert humidex_degree_of_comfort(45.9) == 3
+    assert humidex_degree_of_comfort(46.0) == 4
+    assert humidex_degree_of_comfort(50.0) == 4
