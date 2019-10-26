e587907e0f382a327ca8761bbbeec3e2d3b44a0e
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2019-10-26 21:41:04 +0530

    add discomfort index effect function

--- ladybug_comfort/di.py
+++ ladybug_comfort/di.py
@@ -11,14 +11,55 @@ def discomfort_index(ta, rh):
     temperature.
 
     Note:
-        [1]  Thom, E.C. (1959) "The Discomfort Index". Weatherwise, 12, 57-61. 
+        [1]  Thom, E.C. (1959) "The Discomfort Index". Weatherwise, 12, 57-61.
     Args:
         ta: Air temperature [C]
         rh: Relative humidity  [%]
 
     Returns:
-        hi: Heat index [C]
+        di: Discomfort index [C]
     """
-    di = ta - (0.55 - 0.0055 * rh) * (ta  - 14.5)
+    di = ta - (0.55 - 0.0055 * rh) * (ta - 14.5)
 
     return di
+
+
+def discomfort_index_effect_category(di):
+    """Get the category of effect associated with a given discomfort index
+    (DI).
+
+    Note:
+        The categories have been taken with reference to categories by Kyle,
+        1994 in Unger, 1999.
+
+    Args:
+        di: Discomfort Index
+
+    Returns:
+        category: An integer indicating the level of effect associated with the
+        discomfort index. Values range from -6 to +3.
+    """
+    if di >= 30:
+        category = 3
+    elif di >= 26.5:
+        category = 2
+    elif di >= 20:
+        category = 1
+    elif di >= 15:
+        category = 0
+    elif di >= 13:
+        category = -1
+    elif di >= -1.8:
+        category = -2
+    elif di >= -10:
+        category = -3
+    elif di >= -20:
+        category = -4
+    elif di >= 26.5:
+        category = 2
+    elif di >= -40:
+        category = -5
+    else:
+        category = -6
+
+    return category
