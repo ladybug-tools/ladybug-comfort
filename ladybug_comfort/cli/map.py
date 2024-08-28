35b5eb7429faee2af9ba32675ea115f2bca05f1c
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-08-28 23:25:33 +0200

    fix(map): Add commands for env conditions and data collections

--- ladybug_comfort/cli/map.py
+++ ladybug_comfort/cli/map.py
@@ -22,12 +22,17 @@ from ladybug.datatype.thermalcondition import PredictedMeanVote, \
     ThermalCondition, ThermalConditionElevenPoint
 from ladybug.datatype.temperaturedelta import OperativeTemperatureDelta
 from ladybug.datatype.fraction import Fraction
+from ladybug.datatype.temperature import AirTemperature, \
+    MeanRadiantTemperature, RadiantTemperature
+from ladybug.datatype.temperaturedelta import RadiantTemperatureDelta
+from ladybug.datatype.fraction import RelativeHumidity
 
 from ladybug_comfort.map.irr import irradiance_contrib_map
 from ladybug_comfort.map.mrt import shortwave_mrt_map, longwave_mrt_map
 from ladybug_comfort.map.air import air_map
 from ladybug_comfort.map.tcp import tcp_model_schedules, tcp_total
 from ladybug_comfort.map._enclosure import _parse_enclosure_info, _values_to_data
+from ladybug_comfort.map._helper import restore_original_distribution
 from ladybug_comfort.collection.pmv import PMV, _PMVnoSET
 from ladybug_comfort.collection.adaptive import Adaptive, PrevailingTemperature
 from ladybug_comfort.collection.utci import UTCI
@@ -35,6 +40,7 @@ from ladybug_comfort.collection.utci import UTCI
 from ._helper import load_values, load_analysis_period_str, \
     load_pmv_par_str, load_adaptive_par_str, load_utci_par_str, \
     load_solarcal_par_str, thermal_map_csv, _data_to_ill, set_smallest_dtype
+
 
 _logger = logging.getLogger(__name__)
 
@@ -918,6 +924,107 @@ def tcp(condition_csv, enclosure_info, schedule, occ_schedule_json, folder, log_
         sys.exit(1)
     else:
         sys.exit(0)
+
+
+@map.command('restructure-env-conditions')
+@click.argument(
+    'folder', type=click.Path(exists=True, dir_okay=True, resolve_path=True)
+)
+@click.argument(
+    'dest-folder', type=click.Path(exists=False, dir_okay=True, resolve_path=True)
+)
+@click.argument(
+    'sub-path', type=click.STRING
+)
+def restructure_env_conditions(folder, dest_folder, sub_path):
+    """Read an npy file and convert every row to a data collection.
+
+    This command reads a NumPy array from a npy file and sends it to stdout.
+
+    \b
+    Args:
+        folder: Folder with environmental conditions (initial results).
+        dest_folder: Destination folder for writing the results.
+        sub_path: Sub path for the metric (mrt, air_temperature, longwave_mrt,
+            shortwave_mrt, rel_humidity)-
+    """
+    try:
+        source_folder = os.path.join(folder, sub_path)
+        dest_folder = os.path.join(folder, 'final', sub_path)
+        if sub_path == 'mrt':
+            source_folders = [os.path.join(folder, 'longwave_mrt'),
+                              os.path.join(folder, 'shortwave_mrt')]
+            dest_folders = [os.path.join(folder, 'final', 'longwave_mrt'),
+                            os.path.join(folder, 'final', 'shortwave_mrt')]
+        else:
+            assert os.path.isdir(source_folder), \
+                'Metric "{}" does not exist for this comfort study.'.format(sub_path)
+            source_folders, dest_folders = [source_folder], [dest_folder]
+
+        # restructure the results to align with the sensor grids
+        dist_info = os.path.join(folder, '_redist_info.json')
+        for src_f, dst_f in zip(source_folders, dest_folders):
+            if not os.path.isdir(dst_f):
+                os.makedirs(dst_f)
+                restore_original_distribution(
+                    src_f, dst_f, extension='csv', dist_info=dist_info,
+                    output_extension='csv', as_text=True, fmt='%.12f',
+                    delimiter='comma')
+                grid_info_src = os.path.join(folder, 'grids_info.json')
+                grid_info_dst = os.path.join(dst_f, 'grids_info.json')
+                shutil.copyfile(grid_info_src, grid_info_dst)
+            data_header = create_result_header(folder, os.path.split(dst_f)[-1])
+            result_info_path = os.path.join(dst_f, 'results_info.json')
+            with open(result_info_path, 'w') as fp:
+                json.dump(data_header.to_dict(), fp, indent=4)
+        # if MRT was requested, sum together the longwave and shortwave
+        if sub_path == 'mrt':
+            sum_matrices(dest_folders[0], dest_folders[1], dest_folder)
+            data_header = create_result_header(folder, sub_path)
+            result_info_path = os.path.join(dest_folder, 'results_info.json')
+            with open(result_info_path, 'w') as fp:
+                json.dump(data_header.to_dict(), fp, indent=4)
+    except Exception:
+        _logger.exception('Failed to restructure environmental conditions.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+def create_result_header(env_conds, sub_path):
+    """Create a DataCollection Header for a given metric."""
+    with open(os.path.join(env_conds, 'results_info.json')) as json_file:
+        base_head = Header.from_dict(json.load(json_file))
+    if sub_path == 'mrt':
+        return Header(MeanRadiantTemperature(), 'C', base_head.analysis_period)
+    elif sub_path == 'air_temperature':
+        return Header(AirTemperature(), 'C', base_head.analysis_period)
+    elif sub_path == 'longwave_mrt':
+        return Header(RadiantTemperature(), 'C', base_head.analysis_period)
+    elif sub_path == 'shortwave_mrt':
+        return Header(RadiantTemperatureDelta(), 'dC', base_head.analysis_period)
+    elif sub_path == 'rel_humidity':
+        return Header(RelativeHumidity(), '%', base_head.analysis_period)
+
+
+def sum_matrices(mtxs_1, mtxs_2, dest_dir):
+    """Sum together matrices of two folders."""
+    if not os.path.isdir(dest_dir):
+        os.makedirs(dest_dir)
+    for mtx_file in os.listdir(mtxs_1):
+        if mtx_file.endswith('.csv'):
+            mtx_file1 = os.path.join(mtxs_1, mtx_file)
+            mtx_file2 = os.path.join(mtxs_2, mtx_file)
+            matrix_1 = np.loadtxt(mtx_file1, dtype=np.float32, delimiter=',')
+            matrix_2 = np.loadtxt(mtx_file2, dtype=np.float32, delimiter=',')
+            data = matrix_1 + matrix_2
+            csv_path = os.path.join(dest_dir, mtx_file)
+            np.savetxt(csv_path, data, fmt='%.12f', delimiter=',')
+        elif mtx_file == 'grids_info.json':
+            shutil.copyfile(
+                os.path.join(mtxs_1, mtx_file),
+                os.path.join(dest_dir, mtx_file)
+            )
 
 
 def _tcp_config():
