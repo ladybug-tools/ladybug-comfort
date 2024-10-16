130ad65d192ec4b920f3f04092189d74636b3747
Chris Mackey <chris@ladybug.tools>
2024-10-16 14:52:46 -0700

    ci(dev-deps): Update CI and dev deps to run on Python 3.12

--- ladybug_comfort/cli/_helper.py
+++ ladybug_comfort/cli/_helper.py
@@ -191,21 +191,21 @@ def smallest_integer_dtype(array: np.ndarray):
 
     Args:
         array: NumPy array.
-    
+
     Returns:
         A NumPy integer dtype.
     """
     if np.all(array >= np.iinfo(np.int8).min) and \
-        np.all(array <= np.iinfo(np.int8).max):
+            np.all(array <= np.iinfo(np.int8).max):
         return np.int8
     elif np.all(array >= np.iinfo(np.int16).min) and \
-        np.all(array <= np.iinfo(np.int16).max):
+            np.all(array <= np.iinfo(np.int16).max):
         return np.int16
     elif np.all(array >= np.iinfo(np.int32).min) and \
-        np.all(array <= np.iinfo(np.int32).max):
+            np.all(array <= np.iinfo(np.int32).max):
         return np.int32
     elif np.all(array >= np.iinfo(np.int64).min) and \
-        np.all(array <= np.iinfo(np.int64).max):
+            np.all(array <= np.iinfo(np.int64).max):
         return np.int64
 
 
@@ -221,20 +221,20 @@ def smallest_float_dtype(array: np.ndarray, rtol: float = 1e-5, atol: float = 1e
             is 1e-5.
         atol: The absolute tolerance parameter for `np.allclose`. The default
             is 1e-5.
-    
+
     Returns:
         A NumPy floating dtype.
     """
-    if np.all((array >= np.finfo(np.float16).min) & \
-                (array <= np.finfo(np.float16).max)):
+    if np.all((array >= np.finfo(np.float16).min) &
+              (array <= np.finfo(np.float16).max)):
         if np.allclose(array, array.astype(np.float16), rtol=rtol, atol=atol):
             return np.float16
-    if np.all((array >= np.finfo(np.float32).min) & \
-                (array <= np.finfo(np.float32).max)):
+    if np.all((array >= np.finfo(np.float32).min) &
+              (array <= np.finfo(np.float32).max)):
         if np.allclose(array, array.astype(np.float32), rtol=rtol, atol=atol):
             return np.float32
-    if np.all((array >= np.finfo(np.float64).min) & \
-                (array <= np.finfo(np.float64).max)):
+    if np.all((array >= np.finfo(np.float64).min) &
+              (array <= np.finfo(np.float64).max)):
         if np.allclose(array, array.astype(np.float64), rtol=rtol, atol=atol):
             return np.float64
 
@@ -248,7 +248,7 @@ def smallest_dtype(array: np.ndarray, rtol: float = 1e-5, atol: float = 1e-5):
             is 1e-5. This is also used if the dtype of the array is np.floating.
         atol: The absolute tolerance parameter for `np.allclose`. The default
             is 1e-5. This is also used if the dtype of the array is np.floating.
-    
+
     Returns:
         A NumPy dtype.
     """
@@ -262,14 +262,14 @@ def smallest_dtype(array: np.ndarray, rtol: float = 1e-5, atol: float = 1e-5):
 
 def set_smallest_dtype(array: np.ndarray, rtol: float = 1e-5, atol: float = 1e-5):
     """Return a NumPy array with the smallest possible dtype.
-    
+
     Args:
         array: NumPy array.
         rtol: The relative tolerance parameter for `np.allclose`. The default
             is 1e-5. This is also used if the dtype of the array is np.floating.
         atol: The absolute tolerance parameter for `np.allclose`. The default
             is 1e-5. This is also used if the dtype of the array is np.floating.
-    
+
     Returns:
         A new NumPy array with a smaller dtype.
     """
