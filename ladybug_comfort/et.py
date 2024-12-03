756ef54cd24a4373aee2e6a928aaee4185ad252f
trevor <63176386+TfedUD@users.noreply.github.com>
2024-12-03 13:45:59 -0500

    feat: refactored effective temperature

--- ladybug_comfort/et.py
+++ ladybug_comfort/et.py
@@ -1,41 +1,48 @@
 # -*- coding: utf-8 -*-
 """Effective temperature transitioned from legacy code of @stgeorges"""
 
-def effective_temperature(Ta, ws, rh, SR, ac):
-    # inputs: (Ta, ws, rh, SR, ac):
+def effective_temperature_conditions(tre, te):
+    if tre < 1:
+        effectTE = -4
+    elif tre >= 1 and te < 9:
+        effectTE = -3
+    elif tre >= 9 and te < 17:
+        effectTE = -2
+    elif tre >= 17 and te < 21:
+        effectTE = -1
+    elif tre >= 21 and te < 23:
+        effectTE = 0
+    elif tre >= 23 and te < 27:
+        effectTE = 1
+    elif tre >= 27:
+        effectTE = 2
+
+    return tre, effectTE, []
+    
+
+
+def effective_temperature(ta, ws, rh, sr, ac):
+    """ Inputs: 
+            * ta: Air Temperature 
+            * ws: Wind Speed
+            * rh: Relative Humidity
+            * sr: Solar Radiation
+            * ac: Air Flow
+        Outputs:
+            * tre: Radiant Effective Temperature 
+            * effectTE
+            * Comfort: boolean value for whether or not the effective temp is comfortable
+    """
     if ws <= 0.2:
         # formula by Missenard
-        TE = Ta - 0.4*(Ta - 10)*(1-rh/100)
+        te = ta - 0.4 * (ta - 10) * (1 - rh / 100)
     elif ws > 0.2:
         # modified formula by Gregorczuk (WMO, 1972; Hentschel, 1987)
-        TE = 37 - ( (37-Ta)/(0.68-(0.0014*rh)+(1/(1.76+1.4*(ws**0.75)))) ) - (0.29 * Ta * (1-0.01*rh))
-    
+        te = 37 - ((37 - ta) / (0.68 - (0.0014 * rh) + (1 / (1.76 + 1.4 * (ws ** 0.75))))) - \
+            (0.29 * ta * (1 - 0.01 * rh))
+
     # Radiative-effective temperature
-    TRE = TE + ((1 - 0.01*ac)*SR) * ((0.0155 - 0.00025*TE) - (0.0043 - 0.00011*TE))
-    
-    if TRE < 1:
-        effectTE = -4
-        comfortable = 0
-    elif TRE >= 1 and TE < 9:
-        effectTE = -3
-        comfortable = 0
-    elif TRE >= 9 and TE < 17:
-        effectTE = -2
-        comfortable = 0
-    elif TRE >= 17 and TE < 21:
-        effectTE = -1
-        comfortable = 0
-    elif TRE >= 21 and TE < 23:
-        effectTE = 0
-        comfortable = 1
-    elif TRE >= 23 and TE < 27:
-        effectTE = 1
-        comfortable = 0
-    elif TRE >= 27:
-        effectTE = 2
-        comfortable = 0
-    
-    return TRE, effectTE, comfortable, []
-    
-    
+    tre = te + ((1 - 0.01 * ac) * sr) * ((0.0155 - 0.00025 * te) - (0.0043 - 0.00011 * te))
+
+    return effective_temperature_conditions(tre, te)
     
